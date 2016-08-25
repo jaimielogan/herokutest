431dd356d4872962a32bbd219f179b87a870baa9
jaimielogan <jaimielogan@gmail.com>
2016-08-25 13:31:31 -0600

    Set up .env and server

---
+++ app.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+
+let app = express();
+
+const port = process.env.PORT;
+
+app.get("/", function(req,res){
+  res.json({message:"working!"});
+});
+
+app.listen(port,function(){
+  console.log("I'm listening on port", port);
+});
